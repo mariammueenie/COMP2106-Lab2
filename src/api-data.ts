37170ead000b7b8dad070736df223c25c9f59e05
mariammueenie <mariam_mueen@live.ca>
2026-04-20 10:37:37 -0400

    Create api-data

---
+++ src/api-data.ts
@@ -0,0 +1,22 @@
+import http from 'http';
+import axios from 'axios';
+
+const server = http.createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    try {
+        const apiData = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+
+        res.write('<h2>Sample API Data</h2>');
+        res.write(JSON.stringify(apiData.data));
+    } catch (error) {
+        res.write('<h2>Sample API Data</h2>');
+        res.write('Error retrieving API data');
+    }
+
+    res.end();
+});
+
+server.listen(4000, () => {
+    console.log('Server running at http://localhost:4000');
+});
